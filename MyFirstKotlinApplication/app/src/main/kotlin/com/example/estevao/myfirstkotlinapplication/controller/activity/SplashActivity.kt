35ef13a49ebcd9d290a49484a1e91329e951e17d
Estevao <estevaohecoelho@gmail.com>
2016-09-09 12:05:12 -0300

    improvements
    fix layout errors;
    SnackBar message added;

--- MyFirstKotlinApplication/app/src/main/kotlin/com/example/estevao/myfirstkotlinapplication/controller/activity/SplashActivity.kt
+++ MyFirstKotlinApplication/app/src/main/kotlin/com/example/estevao/myfirstkotlinapplication/controller/activity/SplashActivity.kt
@@ -2,9 +2,11 @@ package com.example.estevao.myfirstkotlinapplication.controller.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import com.bumptech.glide.Glide
 import com.example.estevao.myfirstkotlinapplication.R
 import com.example.estevao.myfirstkotlinapplication.model.entity.Repository
 import com.example.estevao.myfirstkotlinapplication.model.entity.User
@@ -12,6 +14,7 @@ import com.example.estevao.myfirstkotlinapplication.model.http.RequisitionServic
 import com.example.estevao.myfirstkotlinapplication.model.persistence.RepositoryContract
 import com.example.estevao.myfirstkotlinapplication.model.persistence.RepositoryRepository
 import com.example.estevao.myfirstkotlinapplication.model.persistence.UserRepository
+import com.example.estevao.myfirstkotlinapplication.util.isConnected
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -31,32 +34,46 @@ class SplashActivity : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
+        Glide.with(this).load(R.drawable.git_logo).into(git_logo)
+
         buttonEnter.setOnClickListener { view ->
-            startListUserActivity()
+            if (UserRepository.all.size > 0)
+                startListUserActivity()
+            else {
+                setItemsVisibility(View.GONE)
+                Snackbar.make(rootView, "Empty Database!", Snackbar.LENGTH_LONG).setAction("CLOSE", { v ->
+                }).setActionTextColor(resources.getColor(android.R.color.holo_red_light)).show()
+            }
         }
 
         buttonUpdate.setOnClickListener { view ->
             initUserRequest()
+
         }
     }
 
     private fun initUserRequest() {
-        setItemsVisibility(View.VISIBLE)
-        reqService = RequisitionService()
-        reqService.getUsers().enqueue(object : Callback<List<User>> {
-            override fun onResponse(call: Call<List<User>>?, response: Response<List<User>>) {
-                val users = response.body().map { it.copy(id = null) }
-                Log.i("Body: ", users.toString())
-                UserRepository.deleteAll()
-                UserRepository.save(users)
-                initRepositoryRequest(users)
+        if (isConnected(this)) {
+            setItemsVisibility(View.VISIBLE)
+            reqService = RequisitionService()
+            reqService.getUsers().enqueue(object : Callback<List<User>> {
+                override fun onResponse(call: Call<List<User>>?, response: Response<List<User>>) {
+                    val users = response.body().map { it.copy(id = null) }
+                    Log.i("Body: ", users.toString())
+                    UserRepository.deleteAll()
+                    UserRepository.save(users)
+                    initRepositoryRequest(users)
 
-            }
+                }
 
-            override fun onFailure(call: Call<List<User>>?, t: Throwable?) {
-                Log.e("Error: ", t!!.message)
-            }
-        })
+                override fun onFailure(call: Call<List<User>>?, t: Throwable?) {
+                    Log.e("Error: ", t!!.message)
+                }
+            })
+        } else
+            Snackbar.make(rootView, "No internet connection!", Snackbar.LENGTH_LONG).setAction("RETRY", { v ->
+                initUserRequest()
+            }).setActionTextColor(resources.getColor(android.R.color.holo_green_light)).show()
     }
 
 
@@ -95,6 +112,7 @@ class SplashActivity : AppCompatActivity() {
     private fun startListUserActivity() {
         val intent: Intent = Intent(this@SplashActivity, UserListActivity::class.java)
         startActivity(intent)
+        finish()
     }
 }
 
--- MyFirstKotlinApplication/app/src/main/kotlin/com/example/estevao/myfirstkotlinapplication/util/InternetConnectionUtil.kt
+++ MyFirstKotlinApplication/app/src/main/kotlin/com/example/estevao/myfirstkotlinapplication/util/InternetConnectionUtil.kt
@@ -6,15 +6,13 @@ import android.net.ConnectivityManager
 /**
  * Created by estevao on 06/09/16.
  */
-object InternetConnectionUtil {
 
-    fun isConnected(context: Context): Boolean {
-        val conMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork = conMgr.activeNetworkInfo
-        if (activeNetwork != null && activeNetwork.isConnected) {
-            return true
-        } else {
-            return false
-        }
+fun isConnected(context: Context): Boolean {
+    val conMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = conMgr.activeNetworkInfo
+    if (activeNetwork != null && activeNetwork.isConnected) {
+        return true
+    } else {
+        return false
     }
 }
